49a8e4f738447e3ac6fff5d363989e19492454d2
박건우 <pgw04@121.150.124.163>
2026-04-01 00:43:33 +0900

    dffff

--- src/챕터4장실습/WordGameApp.java
+++ src/챕터4장실습/WordGameApp.java
@@ -3,6 +3,7 @@ package 챕터4장실습;
 public class WordGameApp {
 	public static void main(String[] args) {
 		System.out.println("helddlo");
-		System.out.println("byebㅇㄹㅇㅇㄹㅇㄹㄹㅇye");
+		System.out.println("byebㅇㄹㄹㄹㅇye");
+		System.out.println("good night");
 	}
 }
