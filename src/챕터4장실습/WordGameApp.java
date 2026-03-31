9bf0d35211c242ab7702a5e74a573d12b66c1b51
baegofa <parkgw2004@gmail.com>
2026-04-01 01:29:08 +0900

    이제 잘거야

--- src/챕터4장실습/WordGameApp.java
+++ src/챕터4장실습/WordGameApp.java
@@ -2,11 +2,6 @@ package 챕터4장실습;
 
 public class WordGameApp {
 	public static void main(String[] args) {
-		System.out.println("helddlo");
-		System.out.println("byebㅇㄹㄹㄹㅇye");
-		System.out.println("good night");
-		System.out.println("good nigdddhddddt");
-		System.out.println("good morning");
-		System.out.println("이제 된걸까5444");
+		System.out.println("끝이 났도다");
 	}
 }
